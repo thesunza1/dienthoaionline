97ac19798284c22b9b8686ec571a8d8386ae5b52
thesunza1 <quynhtranza1@gmail.com>
2020-11-17 08:17:23 +0700

    add dathang.php

--- functions/dathang.php
+++ functions/dathang.php
@@ -1,4 +1,30 @@
 <?php
-    include '../sesion.php';
-    
-?>
+    include '../php/session.php';
+    date_default_timezone_set("Asia/Ho_Chi_Minh");
+    #get date 
+    $day =date("y-m-d G:i:s" );
+    $mskh = $_SESSION['MSKH'];
+    $sql="insert into  dathang(MSKH,NgayDH,TrangThai) values($mskh,'$day','wait') ";
+    include '../sql/select.php' ;
+
+    $sql = "select SoDonDH from dathang where ngayDH='$day' and MSKH= $mskh";
+    include '../sql/select.php';
+    $row= mysqli_fetch_assoc($result);
+    #lay id cua don hang 
+    $idcart = $row['SoDonDH'];
+    #insert hang trong gio vao chitiethd
+    foreach($_SESSION['products'] as $p) {
+        $sql = "select Gia from HangHoa where MSHH='$p'";
+        include '../sql/select.php';
+        $row= mysqli_fetch_assoc($result);
+        $gia = $row['Gia'];
+        $sql = "insert into chitietdathang values($idcart,$p,1,$gia)";
+        include '../sql/select.php';
+    }
+    #xoa cac san pham trong cart 
+    unset($_SESSION['products']);
+    ?>
+    <script>
+        alert("thành công đặt hàng ");
+        location.href("");
+    </script>
